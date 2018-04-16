e9d2cbe6d472fa5b6e561f8457ee5c8e8ba969bc
Chad Bercea <chadbercea@gmail.com>
2018-04-16 16:32:24 -0400

    update
    lolz

--- hello.c
+++ hello.c
@@ -4,9 +4,9 @@
 int main(int argc, char *argv[])
 {
   if (argc > 1) {
-    printf("Hello %s!\n", argv[1]) ;
+    printf("What it do %s?\n", argv[1]) ;
   } else {
-    printf("Hello world!\n");
+    printf("GTFO!\n");
   }
   return 0;
 }
